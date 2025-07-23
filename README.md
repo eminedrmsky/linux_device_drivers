e3c4ca31031d7371abf10c4d31212379114c7b2b
eminedrmsky <eminedurmuskaya123@gmail.com>
2025-07-23 21:14:48 +0100

    Adds read file operation function.
    Signed-off-by: eminedrmsky <eminedurmuskaya123@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # linux_device_drivers
 Linux device driver development
+
+sudo insmod hello.ko
+
+sudo mknod /dev/hello0 c major_num 0
+
+sudo rmmod hello.ko
+
+ls -lh
+
+https://elixir.bootlin.com/linux/v6.15.7/source/include/linux/fs.h#L2129
+
+
+for guidelines linux-x.x.x/scripts/checkpatch.pl
+
+linux-x.x.x/scripts/checkpatch.pl -f file_path
+
+sudo dmesg -l log_level
+
+sudo dmesg -W
--- hello_kernel/hello.c
+++ hello_kernel/hello.c
@@ -1,13 +1,32 @@
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/fs.h>
+
+static int major_number;
+
+static ssize_t my_read(struct file *f, char __user *c, size_t l, loff_t *o){
+    pr_info("hello_cdev - Read called\n");
+    return 0;
+}
+
+static struct file_operations _file_ops = {
+    .read = my_read
+    };
 
 static int __init my_init(void){
-    printk("Hello kernel\n");
+
+    major_number = register_chrdev(0, "hello_cdev", &_file_ops);
+    if(major_number < 0){
+        pr_err("hello_cdev - Error registering chracter device\n");
+        return major_number;
+    }
+
+    pr_info("hello_cdev - Major number is %d\n", major_number);
     return 0;
 }
 
 static void __exit my_exit(void){
-    printk("Goodbye kernel\n");
+    unregister_chrdev(major_number, "hello_cdev");
 }
 
 module_init(my_init);
